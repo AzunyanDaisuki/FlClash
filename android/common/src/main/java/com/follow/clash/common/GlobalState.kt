8eec861d78ef4850d207b417d50c34425d9624c6
hanzh <maxsun@itx.local>
2026-05-18 09:16:04 +0800

    Sync dev build with oix core

--- android/common/src/main/java/com/follow/clash/common/GlobalState.kt
+++ android/common/src/main/java/com/follow/clash/common/GlobalState.kt
@@ -2,9 +2,8 @@ package com.follow.clash.common
 
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
-import com.google.firebase.FirebaseApp
-import com.google.firebase.crashlytics.FirebaseCrashlytics
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
@@ -37,11 +36,28 @@ object GlobalState : CoroutineScope by CoroutineScope(Dispatchers.Default) {
 
     fun setCrashlytics(enable: Boolean) {
         _application?.let {
-            FirebaseApp.initializeApp(it)
-            FirebaseCrashlytics.getInstance().isCrashlyticsCollectionEnabled = enable
-            if (enable) {
-                log("init crashlytics ${it.processName}")
+            runCatching {
+                initCrashlytics(it, enable)
+                if (enable) {
+                    log("init crashlytics ${it.processName}")
+                }
+            }.onFailure { e ->
+                log("skip crashlytics: ${e.message}")
             }
         }
+    }
+
+    private fun initCrashlytics(context: Context, enable: Boolean) {
+        Class.forName("com.google.firebase.FirebaseApp")
+            .getMethod("initializeApp", Context::class.java)
+            .invoke(null, context)
+
+        val crashlytics = Class.forName("com.google.firebase.crashlytics.FirebaseCrashlytics")
+            .getMethod("getInstance")
+            .invoke(null)
+
+        crashlytics.javaClass
+            .getMethod("setCrashlyticsCollectionEnabled", java.lang.Boolean.TYPE)
+            .invoke(crashlytics, enable)
     }
 }
